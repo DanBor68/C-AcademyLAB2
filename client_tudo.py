7994a3b2b1a31a2271c9a014b14131d6d1dd6189
Antonio Soares Martins <45727250+Somar2018@users.noreply.github.com>
2026-03-03 01:46:21 +0900

    Update client_tudo.py

--- client_tudo.py
+++ client_tudo.py
@@ -1,2 +1,12 @@
 HOST = "127.0.0.1"
 PORT = 8888
+
+tasks = [
+    {"title": "Comprar pão", "done": False},
+    {"title": "Estudar Python", "done": True},
+    {"title": "Enviar email", "done": False}
+]
+
+incomplete = sum(1 for task in tasks if not task["done"])
+
+print("Tarefas incompletas:", incomplete)
